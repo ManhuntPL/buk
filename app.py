4923750a8c9accfff5292c6d45db04c4e32542ef
ManhuntPL <42895428+ManhuntPL@users.noreply.github.com>
2025-01-03 13:11:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from datetime import datetime
     import os
     port = int(os.environ.get("PORT", 10000))
     from flask import Flask
+
+    app = Flask(__name__)
+
 app.run(host="0.0.0.0", port=port)
 
 # Klucz API i adres backendu
